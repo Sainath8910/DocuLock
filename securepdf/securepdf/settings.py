b06825c13c0570feda40f9f40f2fa18908571852
Siva Sainath Reddy <nmachire@gitam.in>
2025-09-22 01:26:30 +0530

    pass

--- securepdf/securepdf/settings.py
+++ securepdf/securepdf/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'securepdf_db',  # your database name
         'USER': 'root',          # your MySQL username
-        'PASSWORD': '8910@Sai',  # your MySQL password
+        'PASSWORD': '****',  # your MySQL password
         'HOST': 'localhost',
         'PORT': '3306',
     }
